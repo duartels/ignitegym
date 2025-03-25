7176308eaaa03c7e3c248737ac41c734f22ff5af
Luan Duarte <luansouza789@gmail.com>
2025-03-25 19:38:06 -0300

    feat: fonts added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home</Text>
