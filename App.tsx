b41ba1d0ddd0d3ee0c756b5b5cbfc3d652e961f1
Luan Duarte <luansouza789@gmail.com>
2025-03-25 19:52:42 -0300

    feat: gluestack added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StatusBar, Text, View } from "react-native";
+import { StatusBar, View } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { GluestackUIProvider, Text } from "@gluestack-ui/themed";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -13,20 +14,24 @@ export default function App() {
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#202024",
-      }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Text>Home</Text>
-    </View>
+    <GluestackUIProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#202024",
+        }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Text color="white" fontSize={34}>
+          Home
+        </Text>
+      </View>
+    </GluestackUIProvider>
   );
 }
